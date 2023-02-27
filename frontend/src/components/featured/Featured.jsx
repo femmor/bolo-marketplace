37cf2e8e2c438bbad0f3240539606deca31719d0
Emmanuel Egomson <fegomson@gmail.com>
2023-02-27 14:23:59 -0600

    Creates the feature section

---
+++ frontend/src/components/featured/Featured.jsx
@@ -0,0 +1,40 @@
+import './Featured.scss';
+
+const Featured = () => {
+  return (
+    <div className="featured">
+      <div className="container">
+        <div className="left">
+          <h1>
+            Find the perfect <span className="freelance">freelance</span>{' '}
+            services for your business
+          </h1>
+          <div className="search">
+            <div className="search-input">
+              <img src="./images/search.png" alt="" />
+              <input
+                type="text"
+                placeholder='Try "Develop an e-commerce app"'
+              />
+            </div>
+            <button>Search</button>
+          </div>
+          <div className="popular">
+            <span>Popular:</span>
+            <button>Website Design</button>
+            <button>WordPress</button>
+            <button>Logo Design</button>
+            <button>AI Services</button>
+          </div>
+        </div>
+        <div className="right">
+          <img src="./images/man.png" alt="featured photo" />
+          <span className="image-name">
+            Bob. <b>Zur</b>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Featured;
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,6 +1,11 @@
+import { Featured } from '../../components';
 import './Home.scss';
 
 const Home = () => {
-  return <div className="home">Homepage</div>;
+  return (
+    <div className="home">
+      <Featured />
+    </div>
+  );
 };
 export default Home;
