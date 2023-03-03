6b94fddda04e5b2258bc58163cecc569e4eca2de
Emmanuel Egomson <fegomson@gmail.com>
2023-03-03 10:58:13 -0600

    Connect to mongoDb database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,31 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectDb from './db/connectDb.js';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
 
 const app = express();
+dotenv.config();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(helmet());
 
 const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const startApp = async () => {
+  try {
+    await connectDb();
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log('Server failed to start!' + error);
+    process.exit(1);
+  }
+};
+
+startApp();
