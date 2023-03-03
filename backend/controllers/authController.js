6073babd17e589c69019e239c7b9e6843ca20e54
Emmanuel Egomson <fegomson@gmail.com>
2023-03-03 15:08:16 -0600

    Encrypts user password and login user

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,9 +1,15 @@
 import User from '../models/userModel.js';
+import bcrypt from 'bcrypt';
 
 // Register a new user
 const register = async (req, res) => {
   try {
-    const newUser = new User(req.body);
+    const hashedPassword = await bcrypt.hash(req.body.password, 5);
+
+    const newUser = new User({
+      ...req.body,
+      password: hashedPassword,
+    });
 
     const user = await newUser.save();
     res.status(200).json(user);
@@ -14,7 +20,25 @@ const register = async (req, res) => {
 
 // Login a user
 const login = async (req, res) => {
+  const { username, password } = req.body;
+
   try {
+    const user = await User.findOne({ username });
+
+    // Check if user exists
+    if (!user) {
+      res.status(404).json({ message: 'User not found!' });
+    }
+
+    // Compare the password
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      res.status(400).json({ message: 'Invalid username or password!' });
+    } else {
+      // Hide the password
+      const { password, ...info } = user._doc;
+      res.status(200).json(info);
+    }
   } catch (error) {
     res.status(500).json({ message: 'Server error!' });
   }
