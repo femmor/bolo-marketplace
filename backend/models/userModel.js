8addd7876c40cffff9d96d4ab169a4b8e8c29c04
Emmanuel Egomson <fegomson@gmail.com>
2023-03-03 13:39:41 -0600

    Designing the DB Structure

---
+++ backend/models/userModel.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: false,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: false,
+    },
+    desc: {
+      type: String,
+      required: false,
+    },
+    isSeller: {
+      type: Boolean,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
